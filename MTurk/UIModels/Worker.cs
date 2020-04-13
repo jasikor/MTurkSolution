dd2da9511e7a74739284ccd00d5e406a00bfa5da
jasikor <jasikor@gmail.com>
2020-04-13 15:13:10 +0200

    worker id data validation

---
+++ MTurk/UIModels/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTurk.UIModels
+{
+    public class Worker
+    {
+        [Required]
+        [StringLength(14, ErrorMessage = "Worker ID is too long, should be 14 characters")]
+        [MinLength(14, ErrorMessage = "Worker ID is too short, should be 14 characters")]
+        public string WorkerID { get; set; }
+    }
+}
