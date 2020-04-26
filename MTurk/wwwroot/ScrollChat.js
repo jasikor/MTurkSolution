c3fe215e3c7ae6f36f18bbdb9b20eac44a3b97d9
jasikor <jasikor@gmail.com>
2020-04-26 10:47:21 +0200

    scroll to bottom added

---
+++ MTurk/wwwroot/ScrollChat.js
@@ -0,0 +1,7 @@
+﻿isFeedAtBottom() {
+    return (this.messageFeed.offsetHeight + this.messageFeed.scrollTop) === this.messageFeed.scrollHeight;
+}
+
+function add(lhs, rhs) {
+    return lhs + rhs;
+}
