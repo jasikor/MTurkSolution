ee7d2c6580eaceb617b9d7d2237d59ca33694295
jasikor <jasikor@gmail.com>
2020-04-27 10:07:53 +0200

    game has the same parameters as gameParameters

--- MTurkDatabase/dbo/Tables/Games.sql
+++ MTurkDatabase/dbo/Tables/Games.sql
@@ -3,7 +3,11 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [SessionId] INT NOT NULL, 
     [StartTime] DATETIME NOT NULL, 
+    [Surplus] INT NOT NULL, 
     [TurksDisValue] INT NOT NULL, 
     [MachineDisValue] INT NOT NULL, 
+    [TimeOut] INT NOT NULL, 
+    [Stubborn] FLOAT NOT NULL, 
+    [MachineStarts] BIT NOT NULL, 
     CONSTRAINT [FK_Games_ToSessions] FOREIGN KEY ([SessionId]) REFERENCES [Sessions]([Id])
 )
