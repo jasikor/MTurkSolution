b388456c5681c6b7a8d5222e1a8cc68717c31e65
jasikor <jasikor@gmail.com>
2020-05-03 12:06:09 +0200

    StartNewGame starts new game according to GameParameters. Still not works when all GameParameters were exhausted

--- MTurk/DataAccess/SessionService.cs
+++ MTurk/DataAccess/SessionService.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MTurk.Models;
 
 namespace MTurk.Data
 {
@@ -57,40 +59,88 @@ namespace MTurk.Data
             return ret;
         }
 
+        /// <summary>
+        /// Starts new game using first not used GameParameter
+        /// </summary>
+        /// <param name="workerId"></param>
+        /// <returns>new game or null if there are no more unused GameParameters</returns>
         public async Task<GameInfo> StartNewGame(string workerId)
         {
+            string sql = @"select top 1  * from GameParameters
+                            left join Games 
+                            on GameParameters.Id = Games.GameParameterId
+                            where Games.SessionId is null";
+
+
+            sql = @"select Top 1 gp.* from GameParameters gp
+                    left join (
+                        select Games.Id, Games.GameParameterId, Games.SessionId 
+                        from Games
+                        left join Sessions
+                        on Sessions.Id = Games.SessionId
+                        where Sessions.WorkerId = @WorkerId) AS G
+                    on gp.Id = G.GameParameterId
+                    where G.SessionId is null;";
+
+
+            GameParametersModel gameParameter = null;
+            try
+            {
+                gameParameter = await _db.LoadData<dynamic, GameParametersModel>(sql, new { WorkerId = workerId });
+            }
+            catch (SqlException e)
+            {
+                Debug.WriteLine(e);
+                throw;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                throw;
+            }
+
+            if (gameParameter is null)
+                return null;
+
             var g = new
             {
                 WorkerId = workerId,
+                GameParameterId = gameParameter.Id,
                 StartTime = DateTime.UtcNow,
-                Surplus = 20,
-                TurksDisValue = 5,
-                MachineDisValue = 5,
-                TimeOut = 66,
-                Stubborn = 0.6,
-                MachineStarts = false,
+                Surplus = gameParameter.Surplus,
+                TurksDisValue = gameParameter.TurksDisValue,
+                MachineDisValue = gameParameter.MachineDisValue,
+                TimeOut = gameParameter.TimeOut,
+                Stubborn = gameParameter.Stubborn,
+                MachineStarts = gameParameter.MachineStarts,
                 Finished = false,
-
             };
-            string sql = @"insert into dbo.Games (SessionId, StartTime, Surplus, TurksDisValue, MachineDisValue, TimeOut, Stubborn, MachineStarts, Finished)
+           sql = @"insert into dbo.Games 
+                              (SessionId,
+                               GameParameterId, StartTime, Surplus, TurksDisValue, MachineDisValue, TimeOut, Stubborn, MachineStarts, Finished)
                            output inserted.*
-                           values ((select Id from Sessions where WorkerId = @WorkerId)
-, @StartTime, @Surplus, @TurksDisValue, @MachineDisValue, @TimeOut, @Stubborn, @MachineStarts, @Finished)";
-
-            var res = await _db.SaveData<dynamic, GameModel>(sql, g);
-
-            ret = new GameInfo()
+                           values 
+                              ((select Id from Sessions where WorkerId = @WorkerId), 
+                               @GameParameterId, @StartTime, @Surplus, @TurksDisValue, @MachineDisValue, @TimeOut, @Stubborn, @MachineStarts, @Finished)";
+            try
             {
-                Id = res.Id,
-                Moves = new List<MoveModel>(),
-                Surplus = res.Surplus,
-                TurksDisValue = res.MachineDisValue,
-                MachineDisValue = res.MachineDisValue,
-                TimeOut = res.TimeOut,
-                Stubborn = res.Stubborn,
-                MachineStarts = res.MachineStarts
-            };
-            return ret;
+               var res = await _db.SaveData<dynamic, GameModel>(sql, g);
+                return new GameInfo()
+                {
+                    Id = res.Id,
+                    Moves = new List<MoveModel>(),
+                    Surplus = res.Surplus,
+                    TurksDisValue = res.MachineDisValue,
+                    MachineDisValue = res.MachineDisValue,
+                    TimeOut = res.TimeOut,
+                    Stubborn = res.Stubborn,
+                    MachineStarts = res.MachineStarts
+                };
+            }
+            catch (SqlException) 
+            {
+               return null;
+            }
         }
 
         private async Task SaveMove(MoveModel move)
