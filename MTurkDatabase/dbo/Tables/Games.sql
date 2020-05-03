4322af146fab6990b781a705e1ae5d9e35683776
jasikor <jasikor@gmail.com>
2020-05-03 11:03:59 +0200

    GameParameterId added

--- MTurkDatabase/dbo/Tables/Games.sql
+++ MTurkDatabase/dbo/Tables/Games.sql
@@ -2,6 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [SessionId] INT NOT NULL, 
+    [GameParameterId] INT NULL,
     [StartTime] DATETIME NOT NULL, 
     [Surplus] INT NOT NULL, 
     [TurksDisValue] INT NOT NULL, 
@@ -11,5 +12,6 @@
     [MachineStarts] BIT NOT NULL, 
     [Finished] BIT NOT NULL, 
     [TurksProfit] INT NULL, 
-    CONSTRAINT [FK_Games_ToSessions] FOREIGN KEY ([SessionId]) REFERENCES [Sessions]([Id])
+    CONSTRAINT [FK_Games_ToSessions] FOREIGN KEY ([SessionId]) REFERENCES [Sessions]([Id]), 
+    CONSTRAINT [FK_Games_ToGameParameters] FOREIGN KEY ([GameParameterId]) REFERENCES [GameParameters]([Id])
 )
