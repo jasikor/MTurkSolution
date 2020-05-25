12110cfa40c69026f3ddc1fdf5901d1c5d1a44f2
jasikor <jasikor@gmail.com>
2020-05-25 08:36:43 +0200

    Game.IsValid() added

--- MTurk/DataAccess/GameInfo.cs
+++ MTurk/DataAccess/GameInfo.cs
@@ -7,5 +7,16 @@ namespace MTurk.Data
         public List<MoveModel> Moves;
         public GameModel Game;
         public bool PartnersAgreed;
+        public bool IsValid()
+        {
+            if (Game.TurksProfit is null)
+                return false;
+            for(int i =0; i < Moves.Count -1; i++)
+            {
+                if (Moves[i].MoveBy == Moves[i + 1].MoveBy)
+                    return false;
+            }
+            return true;
+        }
     }
 }
