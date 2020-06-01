b98fbf2c44fcdc0e896404955f41c26a49095ca0
jasikor <jasikor@gmail.com>
2020-06-01 10:54:21 +0200

    NearestNeighbour implemented.

--- MTurk/Algo/NearestNeighbourMoveEngine.cs
+++ MTurk/Algo/NearestNeighbourMoveEngine.cs
@@ -4,6 +4,8 @@ using NeuralNetworkNET.APIs.Interfaces.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace MTurk.Algo
@@ -11,7 +13,8 @@ namespace MTurk.Algo
     public class NearestNeighbourMoveEngine : IMoveEngine
     {
         private readonly ITrainingDataLoader _dataLoader;
-        private (float[,],float[]) _data;
+        private float[,] X;
+        private float[] Y;
 
         public NearestNeighbourMoveEngine(ITrainingDataLoader dataLoader)
         {
@@ -23,12 +26,48 @@ namespace MTurk.Algo
 
         private void LoadData()
         {
-            _data = _dataLoader.GetRawData();
+            (X, Y) = _dataLoader.GetRawData();
+            distanceIndex = new DistIndex[Y.Length];
         }
 
+        private void CalcDistance(int i, float[] p)
+        {
+            double sum = 0.0;
+            for (int j = 0; j < 11; j++)
+                sum += (X[i, j] - p[j]) * (X[i, j] - p[j]);
+            distanceIndex[i].Distance = Math.Sqrt(sum);
+            distanceIndex[i].Index = i;
+        }
+        private struct DistIndex
+        {
+            public double Distance;
+            public int Index;
+            public override string ToString()
+            {
+                return $"{Index} -> {Distance}";
+            }
+        }
+
+        private DistIndex[] distanceIndex;
         public int GetMachinesOffer(GameInfo g)
         {
+            double s = 0.0;
+            float[] p = new float[11];
+            Parallel.For(0, Y.Length - 1,
+                (i) => { CalcDistance(i, p); }
+                );
+            Array.Sort<DistIndex>(distanceIndex,
+                (x, y) => x.Distance.CompareTo(y.Distance));
+
+            int[] votes = new int[21];
+            int K = 13;
+            for (int i = 0; i < K; i++)
+                votes[
+                (int)(Y[distanceIndex[i].Index] * 21f)
+                ]++;
+
             return 3;
         }
     }
 }
+
